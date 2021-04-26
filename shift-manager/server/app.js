5b713398737abf7239e281e025d0b145ceec07f0
YehonatanHen <yonatan.h333@gmail.com>
2021-04-26 22:15:32 +0300

    packages&commnads initialized

--- shift-manager/server/app.js
+++ shift-manager/server/app.js
@@ -1,1 +1,11 @@
-console.log("hello")
+express = require('express')
+
+const app = express()
+
+port = process.env.PORT || 3001
+
+app.get('/', (req,res) => {
+    res.send("Hello world")
+})
+
+app.listen(port)
