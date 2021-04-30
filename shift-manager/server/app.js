0bef519f7a5d85e5e9f2e929b2806c651ffbab53
YehonatanHen <yonatan.h333@gmail.com>
2021-04-30 07:30:23 +0300

    pkg.json gitignore app.js updated

--- shift-manager/server/app.js
+++ shift-manager/server/app.js
@@ -1,11 +1,18 @@
 express = require('express')
+const path = require('path')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 port = process.env.PORT || 3001
 
-app.get('/', (req,res) => {
-    res.send("Hello world")
+app.get('/*', (req,res) => {
+    res.sendFile('index.html')
 })
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`App is listen to port ${port}`)
+})
+
+module.exports = app
