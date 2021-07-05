e5993663f61f7087c2e0071dfc596a11e23e996d
YehonatanHen <yonatan.h333@gmail.com>
2021-07-06 00:57:35 +0300

    Fixed mongoDB connection

--- shift-manager/server/database/mongodb.js
+++ shift-manager/server/database/mongodb.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 MongoClient.connect(
-	'mongodb+srv://noc:oPUkEnxQ3ZbPhV1y@cluster0.goc5j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	'mongodb+srv://noc:%232QS%2A-F3mwxvUux@cluster0.goc5j.mongodb.net/shift-manager?retryWrites=true&w=majority',
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
@@ -13,6 +13,6 @@ MongoClient.connect(
 		console.log('MongoDB is connected!')
 
 		//Global variable defined and will be reached by all scripts (routers) of App.js in server
-		global.db = client.db('shift-manager')
+		global.db = client.db(process.env.DATABASE_NAME)
 	}
 )
