3ca2ba4531ef26b8d697f7ae6fea0dcd1339ecb5
YehonatanHen <yonatan.h333@gmail.com>
2021-07-22 03:03:43 +0300

    Added dialog and updated teh dataTable

--- shift-manager/src/views/MutualUI/ReportsTable.jsx
+++ shift-manager/src/views/MutualUI/ReportsTable.jsx
@@ -1,32 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { connect } from 'react-redux'
 import { getHeaders } from '../../actions/index'
+import { Dialog } from 'primereact/dialog';
+
 
 const ReportsTable = props => {
-    // let data = null
-    // console.log(props)
-    // if(props.headers.length == 3) {
-    //     data = props.tableData.map((row) => row.slice(0,2))
+    const [selectedRow, selectedRowHandler] = useState(null)
+    const [displayDialog, displayDialogHandler] = useState(false)
+
+    // // const onClick = () => {
+    // //         displayDialogHandler(true)
+    // // }
+
+    // const onHide = () => {
+    //         displayDialogHandler(true)
     // }
+
     return (
-        <DataTable value={props.tableData.map(item => {
-            //Will catch both cases
-            return {
-                'id': item._id,
-                'reporter': item.reporter,
-                'date': item.time,
-                ...item
-            }
-        })}>
-            {props.headers.map((item) => {
-                // if(item === 'date') {
-                //     return <Column field={item} header={item.toUpperCase()} />
-                // }
-                return <Column field={item} header={item.toUpperCase()} />
-            })}
-        </DataTable>
+        <>
+            <DataTable paginator rows={10} stripedRows selectionMode="single" selection={selectedRow} onSelectionChange={e => {
+                selectedRowHandler(e.value)
+                displayDialogHandler(true)
+                console.log(selectedRow) // you can see that when selection is changed, object details printed to the console.
+            }} value={props.tableData.map(item => {
+                //Will catch both cases
+                return {
+                    'id': item._id,
+                    'reporter': item.reporter,
+                    'date': item.time,
+                    ...item
+                }
+            })} >
+                {/* <Column expander style={{ width: '5%' }}/> */}
+                {props.headers.map((item) => {
+                    // if(item === 'date') {
+                    //     return <Column field={item} header={item.toUpperCase()} />
+                    // }
+                    return <Column field={item} sortable header={item.toUpperCase()} />
+                })}
+
+            </DataTable>
+            <Dialog header="Header" visible={displayDialog} style={{ width: '50vw' }} onHide={() => displayDialogHandler(false)}>
+                <p>{selectedRow.id}</p>
+            </Dialog>
+        </>
     )
 }
 
@@ -38,5 +57,7 @@ const mapStatetoProps = (state) => {
 const mapDispatchToProps = {
     getHeaders
 }
+
+
 
 export default connect(mapStatetoProps, mapDispatchToProps)(ReportsTable);
