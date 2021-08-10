4b1acddedf370c2028a91ead43337362c68c0d92
YehonatanHen <yonatan.h333@gmail.com>
2021-08-11 02:50:17 +0300

    Added the edit report screen

--- shift-manager/src/views/create-report-components/createReportTable.jsx
+++ shift-manager/src/views/create-report-components/createReportTable.jsx
@@ -1,15 +1,19 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import axios from 'axios'
 import { connect, useStore } from 'react-redux'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
+import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button'
 import { setAlerts, sendReport } from '../../actions/index'
-import { useCallback } from 'react'
+import EditReportScreen from './editReport'
 
 
 function CreateReportTable(props) {
+	const [selectedRow, selectedRowHandler] = useState(null)
+	const [displayDialog, displayDialogHandler] = useState(false)
 	let id = 0
+
 	const toast = useRef(null);
 
 	const handleSubmit = async () => {
@@ -18,8 +22,8 @@ function CreateReportTable(props) {
 	}
 
 	const accept = () => {
-        toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
-    };
+		toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
+	};
 
 	const footer = (
 		<Button
@@ -35,12 +39,6 @@ function CreateReportTable(props) {
 			<div>
 				<Button
 					type='button'
-					icon='pi pi-search'
-					className='p-button-success'
-					style={{ marginRight: '.5em' }}
-				></Button>
-				<Button
-					type='button'
 					icon='pi pi-pencil'
 					className='p-button-warning'
 				></Button>
@@ -50,7 +48,10 @@ function CreateReportTable(props) {
 
 	return (
 		<>
-			<DataTable
+			<DataTable stripedRows selectionMode="single" onSelectionChange={e => {
+				selectedRowHandler(e.value)
+				displayDialogHandler(true)
+			}}
 				footer={props.alerts.length != 0 ? footer : undefined}
 				value={props.alerts.map((alert) => {
 					return {
@@ -67,17 +68,20 @@ function CreateReportTable(props) {
 					style={{ textAlign: 'center', width: '8em' }}
 				/>
 			</DataTable>
+			<Dialog visible={displayDialog} style={{ width: '90vw' }} onHide={() => displayDialogHandler(false)}>
+				{selectedRow && (<EditReportScreen row={selectedRow} />)}
+			</Dialog>
 		</>
 	)
 }
 
 const mapStateToProps = (state) => ({
-    alerts: state.alertsData,
+	alerts: state.alertsData,
 	user: state.User
 })
 
 const mapDispatchToProps = {
-    setAlerts,
+	setAlerts,
 	sendReport
 }
 
---
+++ shift-manager/src/views/create-report-components/editReport.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditReportScreen = props => {
+    return (
+        <>{'I am the edit report screen'}</>
+    )
+}
+
+export default EditReportScreen
