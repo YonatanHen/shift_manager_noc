aa7bb59b59d08b32ed51ade03c800b0758598bbd
YehonatanHen <yonatan.h333@gmail.com>
2021-08-16 16:00:54 +0300

    Edited the comments

--- shift-manager/src/views/reports-components/reportWindow.jsx
+++ shift-manager/src/views/reports-components/reportWindow.jsx
@@ -1,54 +1,74 @@
 import React from 'react'
 import { Divider } from 'primereact/divider';
 import { Carousel } from 'primereact/carousel'
+import { InputGroup, Button, FormControl } from 'react-bootstrap'
 
 const alertsCarousel = item => {
     if (item) return (
         <>
-        <Divider align="left">
-            <b>{item.title} - {item.time}</b>
-        </Divider>
-        <p>
-            {item.content}
-        </p>
-        {item.comments && item.comments.map((comment, index = 0) => {
-            return (
-                <>
-                    <b>{`comment #${++index}`}</b>
-                    <p>
-                        {comment.content}
-                    </p>
-                </>
-            )
-        })}
-    </>
-    )
-    else return 
-}
-
-const followsCarousel = item => {
-    if (item) return (
-        <>
-        <Divider align="left" />
-        <p>
-            {item.content}
-        </p>
-        {item.comments && item.comments.map((comment, index = 0) => {
-            return (
-                <>
+            <Divider align="left">
+                <b>{item.title} - {item.time}</b>
+            </Divider>
+            <p>
+                {item.content}
+            </p>
+            {/* {item.comments && item.comments.map((comment, index = 0) => {
+                return (
+                    <>
                         <b>{`comment #${++index}`}</b>
                         <p>
                             {comment.content}
                         </p>
                     </>
-            )
-        })}
-    </>
-    ) 
+                )
+            })} */}
+        </>
+    )
+    else return
+}
+
+const followsCarousel = item => {
+    if (item) return (
+        <>
+            <Divider align="left" />
+            <p>
+                {item.content}
+            </p>
+            {/* {item.comments && item.comments.map((comment, index = 0) => {
+                return (
+                    <>
+                        <b>{`comment #${++index}`}</b>
+                        <p>
+                            {comment.content}
+                        </p>
+                    </>
+                )
+            })} */}
+        </>
+    )
+    else return
+}
+
+const commentsCarousel = item => {
+    if (item) return (
+        <>
+            <Divider align="left">
+                <b>{item.responder} - {item.title}</b>
+            </Divider>
+            <p>
+                {item.content}
+            </p>
+
+        </>
+    )
     else return
 }
 
 export const RenderEventsData = props => {
+
+    const editData = () => {
+
+    }
 
     return (
         <>
@@ -64,6 +84,15 @@ export const RenderEventsData = props => {
             <Carousel value={props.row.staging.alerts} itemTemplate={alertsCarousel} numVisible={1} />
             <h6>To Follow:</h6>
             <Carousel value={props.row.staging.follows} itemTemplate={followsCarousel} numVisible={1} />
+            <h6>Comments:</h6>
+            <Carousel value={props.row.comments} itemTemplate={commentsCarousel} numVisible={1} />
+            <InputGroup className="mb-3">
+                <FormControl as="textarea" aria-label="Alert" placeholder="Paste the alert/follow you are commented on here" style={{height: 120}} />
+                <FormControl as="textarea" aria-label="Content" placeholder="Write here the comment" style={{height: 120}} />
+                <Button variant="outline-secondary" id="button-addon2">
+                    Submit Comment
+                </Button>
+            </InputGroup>
         </>
     )
 }
